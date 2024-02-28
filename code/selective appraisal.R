da11c6d53369518fac94b4daa4dcf57e32570080
Robert Ross <raross1217@gmail.com>
2024-02-28 11:50:39 -0600

    WIP

--- code/selective appraisal.R
+++ code/selective appraisal.R
@@ -6,15 +6,18 @@
 # https://ccao-data.github.io/assessr/articles/example-ratio-study.html#detecting-selective-appraisals
 
 ratios <- read_parquet(here::here("cc_appeals", "big data", "ratios.parquet"))  %>%
-  dplyr::filter(year == 2022 & between(mailed_ratio, 0, 3) & is.finite(mailed_ratio)) 
+  dplyr::filter(is.finite(mailed_ratio)) 
 
 
 # Visual detection of sales chasing
 ratios %>%
-ggplot(aes(mailed_ratio)) +
-  stat_ecdf(color = ccao::ccao_colors[1]) +
+  dplyr::filter(year == 2022) %>%
+ggplot() +
+  stat_ecdf(aes(mailed_ratio), color = as.character(ccao::ccao_colors[1])) +
+  stat_ecdf(aes(certified_ratio), color = as.character(ccao::ccao_colors[2])) +
+  stat_ecdf(aes(board_ratio), color = as.character(ccao::ccao_colors[3])) +
   geom_vline(xintercept = 0.98) +
   geom_vline(xintercept = 1.02) +
-  xlim(0.7, 1.3) +
+  xlim(0.8, 1.2) +
   labs(x = "Ratio", y = "F(x)") +
   theme_minimal()
