4cb4cef980e995ee5c49c8a95fad6540ffa0bc53
Robert Ross <raross1217@gmail.com>
2024-03-08 09:37:30 -0600

    Include filters in JSON query

--- code/ingest.R
+++ code/ingest.R
@@ -14,14 +14,10 @@ read.socrata(
 ) %>%
   write_parquet(here::here("cc_appeals", "big data", "appeals.parquet"))
 
-a <- read.socrata(
+read.socrata(
   paste0(
   "https://datacatalog.cookcountyil.gov/resource/nj4t-kc8j.json?$"
-  , "select=pin, triad_name where year >2004")
-) 
-
-a %>% 
+  , "select=pin, triad_name, township_code, neighborhood_code, census_block_geoid where year >2004")
+) %>% 
   dplyr::distinct(pin, triad_name, neighborhood_code, township_code, census_block_geoid) %>%
   write_parquet(here::here("cc_appeals", "big data", "parcel_triads.parquet"))
-
-rm(a)
